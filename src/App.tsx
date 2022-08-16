8ac52320c48ce030c7f180b5144f037787444d89
Dan Elfimov <danilelfimov@MacBook-Air-Dan.local>
2022-08-16 11:32:20 +0300

    feat: initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <div className="main-container">
+        <NavBar />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,12 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar_name">To Do</div>
+      <div className="navbar_settings">Settings</div>
+    </div>
+  );
+};
+
+export default NavBar;
