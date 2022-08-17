9024f976e940c225966d841aa862de6323b76d0d
Dan Elfimov <danilelfimov@MacBook-Air-Dan.local>
2022-08-17 19:41:33 +0300

    fix: added correct api

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -2,12 +2,8 @@ import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import "./News.css";
 
-const API_KEY = process.env.REACT_APP_API_KEY;
-
 const fetchNews = async () => {
-  const res = await fetch(
-    `https://newsapi.org/v2/everything?q=bitcoin&apiKey=${API_KEY}`
-  );
+  const res = await fetch(`http://hp-api.herokuapp.com/api/characters`);
   return res.json();
 };
 
@@ -18,21 +14,12 @@ const News = () => {
 
   useEffect(() => {
     if (status === "success") {
-      data.articles.length = 10;
-      const news = data.articles;
-      const parsedNews = news.map((item: any) => ({
-        title: item.title,
-        content: item.content,
-      }));
-      const strArr = parsedNews.map(
-        (item: { title: string; content: string }) =>
-          `${(item.title, item.content)}`
+      console.log(data);
+      const string = data.reduce(
+        (acc: string, obj: any) => acc + `${obj.name}, `,
+        ""
       );
-      const singleStr = strArr.reduce(
-        (acc: string, item: string) => acc + item,
-        [""]
-      );
-      setNewsState(singleStr);
+      setNewsState(string);
     }
   }, [data, status]);
 
