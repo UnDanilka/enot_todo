b1fcdfd184743e2c14038d3ae72eec4f867c87bf
Dan Elfimov <danilelfimov@MacBook-Air-Dan.local>
2022-08-17 17:58:18 +0300

    feat: added react-query

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -1,36 +1,44 @@
 import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import "./News.css";
 
+const fetchNews = async () => {
+  const res = await fetch(
+    "https://newsapi.org/v2/everything?q=bitcoin&apiKey=28fbe73b30544837bac170f1bdb89308"
+  );
+  return res.json();
+};
+
 const News = () => {
-  const [data, setData] = useState();
+  const { data, status } = useQuery("news", fetchNews);
+  console.log(data);
+
+  const [newsState, setNewsState] = useState();
+
+  useEffect(() => console.log(newsState), [newsState]);
 
   useEffect(() => {
-    fetch(
-      "https://newsapi.org/v2/everything?q=bitcoin&apiKey=28fbe73b30544837bac170f1bdb89308"
-    )
-      .then((res) => res.json())
-      .then((res) => {
-        res.articles.length = 10;
-        const news = res.articles;
-        const parsedNews = news.map((item: any) => ({
-          title: item.title,
-          content: item.content,
-        }));
-        const strArr = parsedNews.map(
-          (item: any) => `${(item.title, item.content)}`
-        );
-
-        const singleStr = strArr.reduce(
-          (acc: any, item: any) => acc + item,
-          [""]
-        );
-        setData(singleStr);
-      });
-  }, []);
+    if (status === "success") {
+      data.articles.length = 10;
+      const news = data.articles;
+      const parsedNews = news.map((item: any) => ({
+        title: item.title,
+        content: item.content,
+      }));
+      const strArr = parsedNews.map(
+        (item: any) => `${(item.title, item.content)}`
+      );
+      const singleStr = strArr.reduce(
+        (acc: any, item: any) => acc + item,
+        [""]
+      );
+      setNewsState(singleStr);
+    }
+  }, [data, status]);
 
   return (
     <div className="marquee">
-      <p>{data}</p>
+      <p>{newsState}</p>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { StoreContextProvider } from "./StoreContext";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <StoreContextProvider>
-    <App />
-  </StoreContextProvider>
+  <QueryClientProvider client={queryClient}>
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
+  </QueryClientProvider>
 );
